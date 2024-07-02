97008815b5db83bdcfd27e981894a36c87fd5101
akerumort <akerumort@gmail.com>
2024-07-02 11:52:07 +0300

    Deleted 'handleValidationExceptions' method

--- src/main/java/com/akerumort/OrderManagementService/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/akerumort/OrderManagementService/exceptions/GlobalExceptionHandler.java
@@ -14,18 +14,6 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-
     @ExceptionHandler(CustomValidationException.class)
     public ResponseEntity<String> handleCustomValidationException(CustomValidationException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
